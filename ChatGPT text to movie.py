bfa370325a3a0410b485ee84bd64400e2b7f5628
George Haber <11169518+GeoHaber@users.noreply.github.com>
2023-03-22 22:28:11 -0400

    Update ChatGPT text to movie.py
    code cleanup and reformatting

--- ChatGPT text to movie.py
+++ ChatGPT text to movie.py
@@ -11,20 +11,21 @@ import requests
 import traceback
 import subprocess
 import numpy as np
-from srt import Subtitle
 
-from typing import List, Optional, Tuple, Union
-from My_Utils import perf_monitor, hm_sz, hm_time
-from gtts import gTTS
-from pydub import AudioSegment
-from PIL import Image
+from srt     import Subtitle
+from typing  import List, Optional, Tuple, Union
+from _utils  import perf_monitor, show_running_message_decorator, hm_sz, hm_time
+from gtts    import gTTS
+from pydub   import AudioSegment
+from PIL     import Image
 from skimage import img_as_ubyte
-from pydub import AudioSegment
+from pydub   import AudioSegment
 
 # Constants
 story_filename = "story.txt"
 num_ChatGPT_images = 1
 size_ChatGPT_image = "512x512"
+size_ChatGPT_image = "1024x1024"
 frame_rate = 30
 
 output_dir = 'output'
@@ -32,8 +33,9 @@ if not os.path.exists(output_dir):
 	os.makedirs(output_dir)
 
 @perf_monitor
+@show_running_message_decorator
 def load_story_from_file(filename: str) -> Optional[List[str]]:
-	''' load text line from stort file '''
+	''' load text line from filename '''
 	msj = sys._getframe().f_code.co_name
 
 	if not os.path.isfile(filename):
@@ -45,18 +47,17 @@ def load_story_from_file(filename: str) -> Optional[List[str]]:
 		return None
 
 	with open(filename, "r") as f:
-		story = f.read().splitlines()
-	msj += f" {story}"
-	logging.info( msj )
-	print( msj )
+		story = [line.strip() for line in f if line.strip()]  # Ignore empty lines or lines with only whitespace
 
 	return story
 
+
 @perf_monitor
+@show_running_message_decorator
 def generate_and_download_image(prompt: str, image_file: str, output_dir: str) -> Optional[str]:
 	'''Generate an image using OpenAI's DALL-E API and download it to a local file.'''
 	msj = sys._getframe().f_code.co_name
-	logging.info(f"{msj} Text: {prompt}")
+#	logging.info(f"{msj} Text: {prompt}")
 
 	try:
 		response = openai.Image.create(
@@ -76,33 +77,43 @@ def generate_and_download_image(prompt: str, image_file: str, output_dir: str) -
 
 		with open(image_file, "wb") as f:
 			f.write(response.content)
-		logging.info(f"Downloaded image {image_file}")
+		logging.info(f" image done {image_file}")
 
 		return image_file
 
 	except openai.error.InvalidRequestError as e:
-		print(f"Request for prompt '{prompt}' was rejected: {e}")
-		logging.error(f"Request for prompt '{prompt}' was rejected by the safety system.")
+		msj = f"Request for prompt '{prompt}' was rejected: {e}"
+		print(msj)
+		logging.error(msj)
 		return None
 	except Exception as e:
-		print(f"An error occurred while generating an image for '{prompt}': {e}")
-		logging.error(f"An error occurred while generating an image for '{prompt}': {e}\n{traceback.format_exc()}")
+		msj = f"An error occurred while generating an image for '{prompt}': {e}\n{traceback.format_exc()}"
+		print (msj)
+		logging.error(msj)
 		return None
 
 @perf_monitor
-def generate_audio(text: str, cnt : int):
+@show_running_message_decorator
+def generate_audio(text: str, out_file: str):
 	''' Audio from text '''
 	msj = sys._getframe().f_code.co_name
-	msj += f" {cnt} Audio from {text}"
+	msj += f" Audio from {text} to {out_file}"
 	logging.info(msj)
 	print (msj)
-	tts = gTTS(text=text, lang='en')
-	audio_file = f"{output_dir}/{cnt:04d}_audio.mp3"
-	tts.save(audio_file)
-	return audio_file
+	try:
+		tts = gTTS(text=text, lang='en')
+		tts.save(out_file)
+		audio = AudioSegment.from_mp3(out_file)
+		au_len = math.ceil(audio.duration_seconds)
+	except Exception as e:
+		msj = f"An error occurred while generating audio from '{text}': {e}\n{traceback.format_exc()}"
+		print (msj)
+
+	return out_file,  au_len
 
 @perf_monitor
-def create_audio_file(audio_files : str, output_dir: str) -> str :
+@show_running_message_decorator
+def combine_all_audio(audio_files : str, output_dir: str) -> str :
 	''' combine audiofiles into one '''
 	msj = sys._getframe().f_code.co_name
 	# Combine audio files
@@ -111,7 +122,6 @@ def create_audio_file(audio_files : str, output_dir: str) -> str :
 	for audio_file in audio_files:
 		audio_segment = AudioSegment.from_file(audio_file)
 		merged_audio = merged_audio + audio_segment
-#        print(audio_file, audio_segment.frame_rate, audio_segment.channels, audio_segment.sample_width)
 	merged_audio.export(merged_audio_file, format="mp3")
 	msj += f" Merged audio file: {merged_audio_file}"
 	logging.info( msj )
@@ -119,69 +129,11 @@ def create_audio_file(audio_files : str, output_dir: str) -> str :
 	return merged_audio_file
 
 @perf_monitor
-def get_audio_length(audio_file: str, extra: int=0 ) -> int:
-	''' Get audio lenght + extra'''
-	msj = sys._getframe().f_code.co_name
-	audio = AudioSegment.from_mp3(audio_file)
-	au_len = math.ceil(audio.duration_seconds + extra)
-	logging.info(f"{msj} Calc Audio length + {extra} = {au_len} sec")
-	print (f"{msj} Calc Audio length + {extra} = {au_len} sec")
-	return au_len
-
-@perf_monitor
-def morph_images(image1: str, image2: str, steps: int = 10, output_dir: str = ".", start_index: int = 0) -> Optional[List[str]]:
-    ''' morph images in number of steps'''
-    msj = sys._getframe().f_code.co_name
-    msj += f" from: {image1} to: {image2} in {steps} Steps"
-    logging.info(msj)
-    print(msj)
-    try:
-        with Image.open(image1) as img1, Image.open(image2) as img2:
-            img1_array = np.array(img1, dtype=np.float32) / 255.0
-            img2_array = np.array(img2, dtype=np.float32) / 255.0
-
-        morphed_images = []
-        for cnt in range(steps + 1):
-            alpha = cnt / steps
-            morphed_image = (1 - alpha) * img1_array + alpha * img2_array
-
-            # Save the morphed image to the disk
-            filename = f"{output_dir}/morphed_image_{start_index + cnt:04d}.png"
-            img = Image.fromarray(img_as_ubyte(morphed_image))
-            img.save(filename)
-            morphed_images.append(filename)
-
-        return morphed_images
-    except Exception as e:
-        logging.error(f"An error occurred while morphing images: {e}\n{traceback.format_exc()}")
-        return []
-
-@perf_monitor
-def create_video_file ( morphed_image_sequences: list , output_file: str ) :
-	''' create a video file from static images '''
-	msj = sys._getframe().f_code.co_name
-	msj += f" Morphed images to video: {output_file}"
-	logging.info (msj)
-	print ( msj )
-
-	msj = sys._getframe().f_code.co_name
-#	cmd = f"ffmpeg -framerate 1 -i {output_dir}/morphed_image_%04d.png -c:v libx264 -r {frame_rate} -pix_fmt yuv420p -y {output_file}"
-#	cmd = f"ffmpeg -framerate {frame_rate} -i {output_dir}/morphed_image_%04d.png -c:v libx265 -pix_fmt yuv420p -r {frame_rate} -y {output_file}"
-	cmd = f"ffmpeg -report -i {output_dir}/morphed_image_%04d.png -safe 0 -c:v libx265 -preset slow -crf 25 -framerate {frame_rate} -y {output_file}"
-	print(f"Executing command: {cmd}")
-	msj += cmd
-	logging.info(msj)
-	print(msj)
-	subprocess.run(cmd, shell=True, check=True)
-
-	return output_file
-
-@perf_monitor
-def create_srt_file(subtitle_entries, output_file):
+def combine_all_subtl(subtitle_entries, output_file):
 	''' Create subtitle bile from subtitiles'''
 	msj = sys._getframe().f_code.co_name
+
 	msj += f" subtitle_entries -> File: {output_file}"
-	logging.info (msj)
 	print ( msj )
 
 	subtitles = []
@@ -189,10 +141,9 @@ def create_srt_file(subtitle_entries, output_file):
 	for index, entry in enumerate(subtitle_entries):
 		duration = datetime.timedelta(seconds=entry["duration"])
 		end_time = start_time + duration
-		content = make_legal_content(entry['text'])
+		content = sanitize_txt(entry['text'])
 		subtitle = Subtitle(index, start_time, end_time, content)
 		subtitles.append(subtitle)
-#		print (f"{index} In= {entry} txt ={entry['text']} Cn: {content} Sub: {subtitle} ")
 		start_time = end_time
 
 	with open(output_file, "w") as f:
@@ -201,24 +152,73 @@ def create_srt_file(subtitle_entries, output_file):
 	return output_file
 
 @perf_monitor
-def add_subtitles_ffmpeg(video_file, srt_file, output_file):
-	''' add subtile to video file '''
-	msj = sys._getframe().f_code.co_name
-	logging.info(f" ffmpeg + subtitle")
-	cmd = f"ffmpeg -i {video_file} -i {srt_file} -vf subtitles={srt_file} -c:v libx264 -c:a copy -y {output_file}"
-	msj += cmd
-	print (msj)
-	logging.info(f"{msj} File: {output_file}")
-	subprocess.call(cmd, shell=True)
-
-@perf_monitor
-def make_legal_content(content):
+def sanitize_txt(content):
 	"""
 	Replace any illegal characters with spaces and remove extra spaces.
 	"""
 	msj = sys._getframe().f_code.co_name
 
 	return re.sub(r'[^\w\s]', ' ', content).strip()
+
+@perf_monitor
+@show_running_message_decorator
+def create_video (video_files : str ='.', aud_all_merged: str ='.', srt_all_merged: str ='.', output_file: str= 'NUL' ) :
+	''' create video'''
+	msj = sys._getframe().f_code.co_name
+	msj += f" Create Video"
+	logging.info(msj)
+	print(msj)
+	try:
+		cmd = f"ffmpeg -report -framerate {frame_rate} -i {output_dir}/morphed_image_%04d.png -i {aud_all_merged} -i {srt_all_merged} \
+			-map 0:v -c:v libx265  -preset slow -crf 25 \
+			-map 1:a -c:a copy     -metadata:s:a:0 language=eng \
+			-map 2:s -c:s mov_text -metadata:s:s:0 language=eng -disposition:s:s:0 default+forced\
+			-y {output_file}"
+
+			# XXX: -vf subtitles={srt_all_merged} could burn the subtitle into the movie  \ # XXX:
+
+#		print(f"\nExecuting command: {cmd}\n")
+		#	print(f"Input file names: {output_dir}/morphed_image_*.png, {aud_all_merged}, {srt_all_merged}")
+		subprocess.run(cmd, shell=True, check=True)
+		msj = f"Video {output_file} Done"
+		print (msj)
+		logging.info (msj)
+		return True
+	except Exception as e:
+		msj = f"An error occurred runninf ffmpeg: {e}\n{traceback.format_exc()}"
+		logging.error(msj)
+		print (msj)
+
+@perf_monitor
+@show_running_message_decorator
+def morph_images(image1: str, image2: str, steps: int = 10, output_dir: str = ".", start_index: int = 0) -> Optional[List[str]]:
+	''' morph images in number of steps'''
+	msj = sys._getframe().f_code.co_name
+	msj += f" from: {image1} to: {image2} in {steps} Steps"
+	logging.info(msj)
+	print(msj)
+	try:
+		with Image.open(image1) as img1, Image.open(image2) as img2:
+			img1_array = np.array(img1, dtype=np.float32) / 255.0
+			img2_array = np.array(img2, dtype=np.float32) / 255.0
+
+		morphed_images = []
+		for cnt in range(steps + 1):
+			alpha = cnt / steps
+			morphed_image = (1 - alpha) * img1_array + alpha * img2_array
+
+			# Save the morphed image to the disk
+			val = start_index + cnt
+			filename = f"{output_dir}/morphed_image_{val:04d}.png"
+			img = Image.fromarray(img_as_ubyte(morphed_image))
+			img.save(filename)
+			morphed_images.append(filename)
+
+		return morphed_images, val +1
+
+	except Exception as e:
+		logging.error(f"An error occurred while morphing images: {e}\n{traceback.format_exc()}")
+		return []
 
 @perf_monitor
 def main(story_file):
@@ -237,8 +237,8 @@ def main(story_file):
 		images.append(image_file)
 
 		# Generate audio measure duration
-		audio_file = generate_audio(line, cnt)
-		duration = get_audio_length(audio_file)
+		audio_file_name = f"{output_dir}/{cnt:04d}_audio.mp3"
+		audio_file, duration = generate_audio(line, audio_file_name)
 		audio_files.append(audio_file)
 
 		# subtitle entries
@@ -253,49 +253,27 @@ def main(story_file):
 		# Morph images
 		morphed_image_sequences = []
 		morph_steps = duration *frame_rate
-		if cnt :
-			morphed_images = morph_images(images[cnt - 1], images[cnt], morph_steps, output_dir, start_index=(cnt - 1) * morph_steps)
+		if cnt == 0 :
+			start_index = 0
+		if cnt > 0:
+			morphed_images, start_index = morph_images(images[cnt - 1], images[cnt], morph_steps, output_dir, start_index )
 			morphed_image_sequences.extend(morphed_images)
 
-	# Create video from images
-	output_file   = f"{output_dir}/_only_video.mp4"
-	morf_img_list = f"{output_dir}/image_list.txt"
-
-
-	with open(morf_img_list, "w") as f:
-		for cnt in range(len(morphed_image_sequences)):
-			abs_image_path = os.path.abspath(f"{output_dir}/morphed_image_{cnt:04d}.png")
-			print (abs_image_path)
-			f.write(f"file '{abs_image_path}'\n")
-	print ( morf_img_list )
-#	cmd = f"ffmpeg -report -i {output_dir}/morphed_image_%04d.png -start_number 0 -c:v libx265 -preset slow -crf 25 -framerate {frame_rate} -y {output_file}"
-	cmd = f"ffmpeg -report -framerate {frame_rate} -i {output_dir}/morphed_image_%04d.png -c:v libx265 -preset slow -crf 25 -vf fps={frame_rate} -y {output_file}"
-
-#	cmd = f"ffmpeg -report -f concat -safe 0 -i {morf_img_list} -c:v libx265 -preset slow -crf 25 -framerate {frame_rate} -y {output_file}"
-	subprocess.run(cmd, shell=True, check=True)
+	print(f"Total all: {len(morphed_image_sequences)}")
 
 	# Merge audio files
 	print(f"\n{len(audio_files)} Audio files")
 	merged_audio = f"{output_dir}/merged_audio.mp3"
-	aud_all_merged = create_audio_file(audio_files, merged_audio)
+	aud_all_merged = combine_all_audio(audio_files, merged_audio)
 
 	# Create subtitle file
 	print(f"\n{len(subtitle_entries)} Srt files")
 	srt_file = f"{output_dir}/subtitles.srt"
-	srt_all_merged = create_srt_file(subtitle_entries, srt_file)
+	srt_all_merged = combine_all_subtl(subtitle_entries, srt_file)
 
 	# Add audio and subtitles to video
-	print(f"\nCobine images Audio and Subtitle files")
 	video_audio_srt = f"{output_dir}/video_audio_srt.mp4"
-	cmd = f"ffmpeg -report -framerate {frame_rate} -i {output_dir}/morphed_image_%04d.png -i {aud_all_merged} -i {srt_all_merged} \
-		-map 0:v -c:v libx265 -preset slow -crf 25 \
-		-map 1:a -c:a copy -metadata:s:a:0 language=eng \
-		-map 2:s -c:s mov_text -metadata:s:s:0 language=eng -disposition:s:s:0 +default+forced \
-		-y {video_audio_srt}"
-	print(f"Executing command: {cmd}")
-	print(f"Input file names: {output_dir}/morphed_image_*.png, {aud_all_merged}, {srt_all_merged}")
-	subprocess.run(cmd, shell=True, check=True)
-
+	all_good = create_video ( f"{output_dir}/morphed_image_%04d.png", aud_all_merged, srt_all_merged, video_audio_srt )
 
 if __name__ == "__main__":
 	# XXX: Setup the loggigng file
